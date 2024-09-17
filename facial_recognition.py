da54000a39082de91c30689bd4191b368937ccb4
CarlNegrescu <carl.negrescu@hotmail.com>
2024-09-17 13:12:25 -0700

    added some known_faces for the recognition, encountered some error, will fix in the next commit

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,4 +1,5 @@
 import face_recognition
+import face_recognition_models
 import cv2
 import numpy as np
 
@@ -15,11 +16,11 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
+obama_image = face_recognition.load_image_file("known_faces/obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
+biden_image = face_recognition.load_image_file("known_faces/biden.jpg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
