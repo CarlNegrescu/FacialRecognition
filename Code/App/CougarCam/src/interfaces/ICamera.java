6c7387a2a8fdfdccc78ef7dde96af077ce3f05c7
CarlNegrescu <carl.negrescu@hotmail.com>
2024-11-04 19:25:34 -0800

    Added the Resource class

--- Code/App/CougarCam/src/interfaces/ICamera.java
+++ Code/App/CougarCam/src/interfaces/ICamera.java
@@ -1,4 +1,34 @@
 package interfaces;
 
-public class ICamera {
+import utils.Resource;
+
+interface ICamera
+{
+  /**
+   * Initializes the Camera Module for use
+   *
+   * @return The success of the operation
+   */
+  public Resource.Result init();
+
+  /**
+   * Opens the camera for use, Uses a JFrame Panel for use
+   *
+   * @return The Result of the operation
+   */
+  public Resource.Result openCamera(String windowName);
+
+  /**
+   * Closes the camera
+   *
+   * @return The Result of the operation
+   */
+  public Resource.Result closeCamera();
+
+  /**
+   *
+   * @return The Result of the operation
+   */
+  public Resource.Result takePicture();
+
 }
