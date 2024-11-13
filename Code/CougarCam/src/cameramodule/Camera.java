29749511c0fe882568b59d3f10508c63c6f90ba0
CarlNegrescu <carl.negrescu@hotmail.com>
2024-11-13 09:10:05 -0800

    Got the implemetation of the facial detection in another thread to work

--- Code/CougarCam/src/cameramodule/Camera.java
+++ Code/CougarCam/src/cameramodule/Camera.java
@@ -26,15 +26,50 @@ public class Camera extends Thread
    */
   public Camera (int index)
   {
-    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    _camera = new VideoCapture(index);
-    _frame = new Mat();
-    _cascade = new CascadeClassifier("resources/haarcascades/haarcascade_frontalface_alt.xml");
-    _faceDetections = new MatOfRect();
-    _cropFace = new Mat();
+	  _camera = new VideoCapture(0);
+	  _frame = new Mat();
+	  _cascade = new CascadeClassifier("resources/haarcascades/haarcascade_frontalface_alt.xml");
+	  System.out.print("Created the Classifer");
+	  _faceDetections = new MatOfRect();
+	  _cropFace = new Mat();
 
+   
   }
   
+  
+  public void runCamera(VideoCapture cap)
+  {
+	  while (cont)
+	  {
+		  _camera.read(_frame);
+		
+
+		  if (_frame.empty())
+		  {
+			  System.out.println("Frame is empty");
+			  break;
+		  }
+//		  Imgproc.cvtColor(_frame, _frame, Imgproc.COLOR_RGB2GRAY);
+//		  Imgproc.equalizeHist(_frame, _frame);
+		  _cascade.detectMultiScale(_frame, _faceDetections);
+		  
+		  for (Rect rect : _faceDetections.toArray())
+		  {
+			  Imgproc.rectangle(_frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
+			  _cropFace = new Mat(_frame, rect);
+			  ///send to Facial Rec
+			  ///Then Delete object and repeat
+		  }
+		  HighGui.imshow("CameraFeed", _frame);
+		  if (HighGui.waitKey(MAX_PRIORITY) == 'q')
+		  {
+			  _camera.release();
+			  break;
+		  }
+	  }
+	  HighGui.destroyAllWindows();
+	  }
+
   /**
    * @brief implements the java thread library function
    */
@@ -43,16 +78,16 @@ public class Camera extends Thread
     while (cont)
     {
       _camera.read(_frame);
-
+      System.out.println("Read in Frame");
       if (_frame.empty())
       {
         System.out.println("Frame is empty");
         break;
       }
-      Imgproc.cvtColor(_frame, _frame, Imgproc.COLOR_RGB2GRAY);
-      Imgproc.equalizeHist(_frame, _frame);
+//      Imgproc.cvtColor(_frame, _frame, Imgproc.COLOR_RGB2GRAY);
+//      Imgproc.equalizeHist(_frame, _frame);
       _cascade.detectMultiScale(_frame, _faceDetections);
-
+      System.out.println("Detected Face");
       for (Rect rect : _faceDetections.toArray())
       {
         Imgproc.rectangle(_frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
@@ -61,7 +96,15 @@ public class Camera extends Thread
         ///Then Delete object and repeat
       }
       HighGui.imshow("CameraFeed", _frame);
+      if (HighGui.waitKey(MAX_PRIORITY) == 'q')
+	  {
+    	  System.out.println("Exit Key detected");
+		  _camera.release();
+		  break;
+	  }
     }
+    HighGui.destroyAllWindows();
+    System.out.println("Destroying Windows");
   }
 
   /**
--- Code/CougarCam/src/cougarCam/Main.java
+++ Code/CougarCam/src/cougarCam/Main.java
@@ -1,32 +1,63 @@
 package cougarCam;
 
-import cameramodule.Camera;
 import org.opencv.core.*;
-import org.opencv.imgproc.Imgproc;
 import org.opencv.highgui.HighGui;
-import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.videoio.VideoCapture;
-import utils.Resource;
-import java.lang.Thread;
-import static org.opencv.imgcodecs.Imgcodecs.imwrite;
-
+import cameramodule.Camera;
 
 
 public class Main
 {
   public static void main(String[] args)
   {
-    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-    VideoCapture capture = new VideoCapture(0);
-    Mat frame = new Mat();
-    while (true)
-    {
-      capture.read(frame);
-      HighGui.imshow(null, frame);
-      if(HighGui.waitKey(1) == 'q')
-        break;
-      
-    }
+	  System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	  VideoCapture cap = new VideoCapture(0);
+	  Camera cam = new Camera(0);
+	  cam.run();
+	  
+	  
+	  
+	  
+	  
   }
+//    System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//    VideoCapture _camera = new VideoCapture(0);
+//    Mat _frame = new Mat();
+//    CascadeClassifier _cascade = new CascadeClassifier("resources/haarcascades/haarcascade_frontalface_alt.xml");
+//    MatOfRect _faceDetections = new MatOfRect();
+//    Mat _cropFace = new Mat();
+//    
+//    
+//    
+////    
+//      while (true)
+//      {
+//    	_camera.read(_frame);
+////
+//      if (_frame.empty())
+//      {
+//        System.out.println("Frame is empty");
+//        break;
+//      }
+//      _cascade.detectMultiScale(_frame, _faceDetections);
+//
+//      for (Rect rect : _faceDetections.toArray())
+//      {
+//        Imgproc.rectangle(_frame, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0), 2);
+//        _cropFace = new Mat(_frame, rect);
+//        ///send to Facial Rec
+//        ///Then Delete object and repeat
+//      }
+//    	HighGui.imshow("CameraFeed", _frame);
+//    	if (HighGui.waitKey(1) == 'q') 
+//    	{
+//    		_camera.release();
+//    		break;
+//    	}
+//      }
+//      HighGui.destroyAllWindows();
+//    
+//  }
 }
